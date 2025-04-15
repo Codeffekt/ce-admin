d5f6df7f39a1786689bc16864c79f064fd071371
fabien <fabien.pelisson@codeffekt.com>
2025-04-15 11:08:54 +0200

    fix: import

--- projects/lib/src/lib/spaces/ce-admin-spaces.module.ts
+++ projects/lib/src/lib/spaces/ce-admin-spaces.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from "@angular/core";
 import { CeAdminSpacesRoutingModule } from "./ce-admin-spaces-routing.module";
 import { MainHomeModule } from "../main/main-home";
-import { CeFormDataService, ListItemStoreService, SpaceFormPathService } from "@codeffekt/ce-core";
+import { CeFormDataService, ListItemStoreService } from "@codeffekt/ce-core";
 import { FormDataService } from "../services/form-data.service";
 import { FormSpaceEditorFormat } from "@codeffekt/ce-core-data";
 import { ListItemSpaceComponent } from "./list-item-space";
