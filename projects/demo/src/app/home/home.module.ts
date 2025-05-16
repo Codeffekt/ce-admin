a48603e4ecd5f69c9bbd5e5814a4d7e99184f80f
fabien <fabien.pelisson@codeffekt.com>
2025-05-16 20:11:18 +0200

    feat: add editor module

--- projects/demo/src/app/home/home.module.ts
+++ projects/demo/src/app/home/home.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
+import { CeCodeEditorModule } from '@codeffekt/ce-code-editor';
 @NgModule({
   imports: [
     CommonModule,
+    CeCodeEditorModule,
     HomeRoutingModule,    
   ],
   providers: [       
