fa67c061b107df15aa540532a88c267a39dceabd
fabien <fabien.pelisson@codeffekt.com>
2025-06-04 13:38:11 +0200

    fix ceformsmodule missing

--- projects/demo/src/app/home/home.module.ts
+++ projects/demo/src/app/home/home.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { CeCodeEditorModule } from '@codeffekt/ce-code-editor';
+import { CeFormsModule } from '@codeffekt/ce-core';
 @NgModule({
   imports: [
     CommonModule,
     CeCodeEditorModule,
-    HomeRoutingModule,    
+    HomeRoutingModule,   
+    CeFormsModule, 
   ],
   providers: [       
   ],
